3aa5f90a8179efcb3569da5c4224c759f9ebc493
Hesam <hesamhrf@gmail.com>
2024-12-27 03:20:08 +0330

    Update chest

--- MindMaze/Assets/_Scripts/Chest.cs
+++ MindMaze/Assets/_Scripts/Chest.cs
@@ -1,27 +1,70 @@
 ﻿using UnityEngine;
+using DG.Tweening;
 
 public class Chest : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField] private GameObject keyPrefab;
+
+    [Header("Animation Settings")]
+    [SerializeField] private float keyFloatDuration = 1f;
+    [SerializeField] private float keyFloatHeight = 1f;
+    [SerializeField] private Vector3 keyRotation = new Vector3(0, 360, 0);
+
     private bool isPlayerNearby = false;
     private Player player;
+    private Animator animator;
+    private bool isOpen = false;
+    private AudioSource chestAudioSource;
 
     private void Start()
     {
-        // Find the player in the scene
         player = FindObjectOfType<Player>();
+        animator = GetComponent<Animator>();
+        chestAudioSource = GetComponent<AudioSource>();
     }
 
     private void Update()
     {
-        if (isPlayerNearby && Input.GetKeyDown(KeyCode.E))
+        if (isPlayerNearby && Input.GetKeyDown(KeyCode.E) && !isOpen)
         {
-            if (player != null)
-            {
-                player.FindKey();
-                Debug.Log("Chest opened and key added!");
-                Destroy(gameObject); // Destroy the chest after opening
-            }
+            OpenChest();
         }
+    }
+
+    private void OpenChest()
+    {
+        if (player != null)
+        {
+            if (chestAudioSource != null)
+            {
+                chestAudioSource.Play();
+            }
+            animator.SetTrigger("Open");
+            isOpen = true;
+            SpawnAndAnimateKey();
+        }
+    }
+
+    private void SpawnAndAnimateKey()
+    {
+        // Spawn the key at chest position
+        GameObject key = Instantiate(keyPrefab, transform.position, Quaternion.identity);
+
+        // Create a sequence of animations
+        Sequence keySequence = DOTween.Sequence();
+
+        // First float up and rotate
+        keySequence.Append(key.transform.DOMoveY(transform.position.y + keyFloatHeight, keyFloatDuration)
+            .SetEase(Ease.OutQuad));
+        keySequence.Join(key.transform.DORotate(keyRotation, keyFloatDuration, RotateMode.FastBeyond360));
+
+        // When complete, destroy the key object and update the player's key count
+        keySequence.OnComplete(() => {
+            player.FindKey();
+            Destroy(key);
+            Debug.Log("Chest opened and key added!");
+        });
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
---
+++ MindMaze/Assets/_Scripts/PauseMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject currentCanv;
+    public GameObject weapon;
+    public GameManager gameManager;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        currentCanv.SetActive(true);
+        weapon.SetActive(true);
+        gameManager.SetCursorIcon();
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        currentCanv.SetActive(false);
+        weapon.SetActive(false);
+        gameManager.SetCursorDefault();
+        Time.timeScale = 0;
+        gameIsPaused = true;
+    }
+
+    public void loadMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void quiteGame()
+    {
+        Application.Quit();
+    }
+}
