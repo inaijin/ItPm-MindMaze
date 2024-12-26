cf1611d1861826fca7984b6218aed097178da632
inaijin <kouroshsajjadi@yahoo.com>
2024-12-26 21:55:26 +0330

    add Main & Pause Menu

---
+++ MindMaze/Assets/PauseMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject currentCanv;
+    public GameObject weapon;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        currentCanv.SetActive(true);
+        weapon.SetActive(true);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        currentCanv.SetActive(false);
+        weapon.SetActive(false);
+        Time.timeScale = 0;
+        gameIsPaused = true;
+    }
+
+    public void loadMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void quiteGame()
+    {
+        Application.Quit();
+    }
+}
--- MindMaze/Assets/_Scripts/GameManager.cs
+++ MindMaze/Assets/_Scripts/GameManager.cs
@@ -15,9 +15,14 @@ public class GameManager : MonoBehaviour
         SetCursorIcon();
     }
 
-    private void SetCursorIcon()
+    public void SetCursorIcon()
     {
         Cursor.SetCursor(cursorTexture, new Vector2(cursorTexture.width / 2f, cursorTexture.height / 2f), CursorMode.Auto);
+    }
+
+    public void SetCursorDefault()
+    {
+        Cursor.SetCursor(null, new Vector2(cursorTexture.width / 2f, cursorTexture.height / 2f), CursorMode.Auto);
     }
 
     public void RestartLevel()
